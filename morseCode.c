a7ff6c0ce2bc5685f243003572ac7845f2a14ac3
Joshua Obiha <joshua.obiha@hotmail.com>
2019-01-29 16:17:49 -0500

    Update morseCode.c

--- morseCode.c
+++ morseCode.c
@@ -1,67 +1,52 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 
-#define MAX  26
-#define STRING_SIZE 1000
-
+#define MAX         27
+#define STRING_SIZE 27
 
 int main(){
 
-char morse[MAX][STRING_SIZE] = {
-".-", //A
-"-...",//B
-"-.-.",//C
-"-..",//D
-".",//E
-"..-.",//F
-"--.",//G
-"....",//H
-"..",//I
-".---",//J
-"-.-",//K
-".-..",//L
-"--",//M
-"-.",//N
-"---",//O
-".--.",//P
-"--.-",//Q
-".-.",//R
-"...",//S
-"-",//T
-"..-",//U
-"...-",//V
-".--",//W
-"-..-",//X
-"-.--",//Y
-"--..",//Z
-
-};
+char *morse[2][28] = {{".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--..","   "},
+                    {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"," "}};
 
 
-printf("Enter the sentence to be converted:");
-
-//GETTING USER'S WORD/SENTENCE
-
-
+//GETTING USER's word/sentence
+printf("Enter the sentence to be converted:\n");
 char userInput[STRING_SIZE];
-
 scanf("%[^\n]", userInput);
-
 
 
 printf("\n");
 
-printf("Here is your sentence in Morse Code:");
-printf("%s\n", userInput);
+//CONVERTS USER's word/sentence to UPPERCASE
+for(int i = 0; i < strlen(userInput); i++){
+    if(userInput[i] >= 'a' && userInput[i] <= 'z'){userInput[i] = userInput[i] - 32;}}
+
+printf("ALL CAPS: %s\n", userInput);
 
 
+int size = sizeof(morse[0])/sizeof(morse[0][1]);
 
-printf("SIZE OF SENTENCE: %lu\n", strlen(userInput));
+printf("%d\n", size);
+
+// // printf("Here is your sentence in Morse Code:");
+// for(int i = 0; i < strlen(userInput); i++){
+// for(int j = 0; j < sizeof(morse[0])/sizeof(morse[0][0]); j++){
+//     if(strcmp(userInput[i], morse[1][0]) == 0){
+//         printf("%s", morse[0][j]);
+//     }
+
+//         // printf("%c", userInput[i]);
+
+// }
+// }
 
 
-printf("SIZE OF MORSE ARRAY %lu\n", sizeof(morse)/sizeof(morse[0]));
-
+if(strcmp(userInput[0], morse[1][0]) == 0){
+    printf("This works");
+}
 
 
 return 0;
