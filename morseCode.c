a9c05fd464bcb270dea2bd2287cc31428c4038d6
Joshua Obiha <joshua.obiha@hotmail.com>
2019-02-03 00:08:28 -0500

    Update morseCode.c

--- morseCode.c
+++ morseCode.c
@@ -4,52 +4,73 @@
 
 
 #define MAX         27
-#define STRING_SIZE 27
+#define STRING_SIZE 1000
+#define CONVERT     32
 
 int main(){
 
-char *morse[2][28] = {{".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--..","   "},
-                    {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"," "}};
+    char  userInputArr [STRING_SIZE];
+    char* morse        [29]           =  {".- ","-... ","-.-. ","-.. ",". ","..-. ","--. ",".... ",".. ",".--- ","-.- ",".-.. ","-- ","-. ","--- ",".--. ","--.- ",".-. ","... ","- ","..- ","...- ",".-- ","-..- ","-.-- ","--.. ","  ","? "};
+    char  alphabet     [29]           =  {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',' ','?'};
+    char  userInput    [STRING_SIZE];
 
 
-//GETTING USER's word/sentence
-printf("Enter the sentence to be converted:\n");
-char userInput[STRING_SIZE];
-scanf("%[^\n]", userInput);
+    printf("Enter the sentence to be converted:\n");
+    scanf("%[^\n]", userInput);
+
+    // printf("String: %s\n", userInput);
+
+    //UPPER CASE
+    for(int i = 0; i < strlen(userInput); i++){
+        if(userInput[i] >= 'a' && userInput[i] <= 'z'){
+            userInput[i] = userInput[i] - 32;}
+        
+        if(userInput[i] >= '0' && userInput[i] <= '9'){
+            userInput[i] = '?';
+        }
+        if(userInput[i] >= '!' && userInput[i] <= '/'){
+            userInput[i] = '?';
+        }
+        if(userInput[i] >= ':' && userInput[i] <= '@'){
+            userInput[i] = '?';
+        }
+        if(userInput[i] >= '[' && userInput[i] <= '`'){
+            userInput[i] = '?';
+        }
+         if(userInput[i] >= '{' && userInput[i] <= '~'){
+            userInput[i] = '?';
+        }
+        }
 
 
-printf("\n");
+    // printf("String: %s\n", userInput);
+    
+ 
 
-//CONVERTS USER's word/sentence to UPPERCASE
-for(int i = 0; i < strlen(userInput); i++){
-    if(userInput[i] >= 'a' && userInput[i] <= 'z'){userInput[i] = userInput[i] - 32;}}
+    printf("\n");
+    printf("Here is your sentence in Morse Code:\n");
+    char *str = userInputArr;
 
-printf("ALL CAPS: %s\n", userInput);
-
-
-int size = sizeof(morse[0])/sizeof(morse[0][1]);
-
-printf("%d\n", size);
-
-// // printf("Here is your sentence in Morse Code:");
-// for(int i = 0; i < strlen(userInput); i++){
-// for(int j = 0; j < sizeof(morse[0])/sizeof(morse[0][0]); j++){
-//     if(strcmp(userInput[i], morse[1][0]) == 0){
-//         printf("%s", morse[0][j]);
-//     }
-
-//         // printf("%c", userInput[i]);
-
-// }
-// }
+    for (int i = 0; i < strlen(userInput); i++){str[i] = userInput[i] ;}
 
 
-if(strcmp(userInput[0], morse[1][0]) == 0){
-    printf("This works");
-}
+    // for (int i = 0; i < strlen(userInput); i++){printf("%c", str[i]);}
+
+     for(int j = 0; j < strlen(userInput); j++){
+         for( int i = 0; i < sizeof(morse)/sizeof(morse[0]); i++){
+              if(str[j] == alphabet[i]){
+                printf("%s", morse[i]);}
+                
+         }
+        
+     }
 
 
-return 0;
+      printf("\n");
+      printf("\n");
+
+    
+    return 0;
 
 
 }
